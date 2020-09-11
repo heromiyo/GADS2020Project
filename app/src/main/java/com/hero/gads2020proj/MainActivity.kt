d62a05a17c95cff2d643d4cf24982547158d9817
Francis Miyoba <francismiyoba@gmail.com>
2020-09-11 11:49:04 +0200

    Add dependencies

---
+++ app/src/main/java/com/hero/gads2020proj/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.hero.gads2020proj
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
